4b0fc0339e4a2d9b531b8fed52dc45f223ee45e4
ceccobell <francescobell2001@gmail.com>
2024-07-02 15:17:36 +0200

    [UPD] exercise completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-alert("TEsts")
+const ingredienti = ['guanciale', 'peperoni', 'pasta', 'olive', 'controfiletto', 'quaglie', 'insalata', 'gamberi', 'pollo', 'pomodori']
+
+const lista_spesa = document.getElementById("lista-spesa")
+
+let i = 0
+
+while (i < ingredienti.length) {
+
+    const li = document.createElement("li")
+
+    li.innerText = ingredienti[i]
+
+    lista_spesa.append(li)
+
+    i++
+}
